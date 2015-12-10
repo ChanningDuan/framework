40d34bbfc375baaeb9d36b75764adbef96990ac8
Nick G <gejadze@gmail.com>
2015-12-10 12:52:42 -0500

    version bump

--- library/ngfw/Webquery.php
+++ library/ngfw/Webquery.php
@@ -29,7 +29,7 @@ namespace ngfw;
  *
  * @package       ngfw
  * @subpackage    library
- * @version       1.3.6
+ * @version       1.3.7
  * @copyright (c) 2015, Nick Gejadze
  */
 class Webquery {
